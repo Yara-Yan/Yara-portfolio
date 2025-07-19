df8d310fcc61fbdacfe1eddfd0f109076c95c844
Yara-Yan <138326473+Yara-Yan@users.noreply.github.com>
2025-07-19 11:05:08 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -98,3 +98,33 @@
     });
 
 })(jQuery);
+
+<!-- Swiper JS CDN -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+<script>
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    breakpoints: {
+      768: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      }
+    }
+  });
+
+  // Click to enlarge
+  function openFullscreen(img) {
+    const win = window.open("");
+    win.document.write("<img src='" + img.src + "' style='width:100%'>");
+  }
+</script>
+
